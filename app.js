0991f5c9d1e55f1b156b91fc92b42ad89ea4437e
nguyenanhcd <anh487303@gmail.com>
2024-07-24 15:59:49 +0700

    use mongodb

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
 const app = express();
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,12 +17,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // when you run npm start, nodejs only run the code generator but not the middleware
 app.use((req, res, next) => {
-  User.findByPk(1)
+  User.findById('669f5c4288385f216bc27a82')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch((err) => next(err));
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.use('/admin', adminRoutes);
@@ -37,34 +32,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: 'John Doe', email: 'john.doe@example.com' });
-    }
-    return user;
-  })
-  .then((user) => {
-    user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect(() => {
+  console.log('Server is running on port 3000...');
+  app.listen(3000);
+});
--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('nodejsudemy', 'root', '270605', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+let _db;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://levektor74:Zxcvbnm123@cluster0.csfs5va.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then((client) => {
+      _db = client.db();
+      console.log('Connected !!!');
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+const getDb = () => {
+  if (_db) return _db;
+  throw new Error('No database connection');
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
